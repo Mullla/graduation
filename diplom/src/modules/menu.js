ccdf332f7db7a75b87808835eef64497aac3c29e
Mars <diablo_13_06@mail.ru>
2021-03-22 23:45:36 +0300

    added closing option

--- diplom/src/modules/menu.js
+++ diplom/src/modules/menu.js
@@ -1,18 +1,33 @@
 const showMenu = () => {
     const btnMenu = document.querySelector('.menu__icon'),
-        blockMenu = document.querySelector('.popup-dialog-menu');
+        blockMenu = document.querySelector('.popup-dialog-menu'),
+        btnClose = blockMenu.querySelector('.close-menu');
 
         btnMenu.addEventListener('click', () => {
 
             if (document.documentElement.clientWidth < 576){
+                // выплывает сверху
                 blockMenu.classList.toggle('showHide-menu');
             } else {
-                blockMenu.classList.toggle('showMenu');
+                blockMenu.classList.add('showMenu');
+                blockMenu.classList.remove('hideMenu');
+                // чтобы меню осталось в выдвинутом состоянии
+                blockMenu.style.animationFillMode = 'forwards';
+                
+            }
+
+        });
+
+        btnClose.addEventListener('click', () => {
+            if (document.documentElement.clientWidth < 576){
+                // выплывает сверху
+                blockMenu.classList.toggle('showHide-menu');
+            } else {
+                blockMenu.classList.remove('showMenu');
+                blockMenu.classList.add('hideMenu');
                 // чтобы меню осталось в выдвинутом состоянии
                 blockMenu.style.animationFillMode = 'forwards';
             }
-
-            
         });
 }
 
