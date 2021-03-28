b72e00b837ca90f80ffeebfb1bd85bdadccd8f7a
Mars <diablo_13_06@mail.ru>
2021-03-28 13:23:22 +0300

    main section is ok

---
+++ diplom/src/modules/sections/main.js
@@ -0,0 +1,13 @@
+const main = () => {
+    const btnArrow = document.querySelector('.header-contacts__arrow'),
+        bottomPhoneNumber = document.querySelector('.header-contacts__phone-number-accord');
+
+    btnArrow.addEventListener('click', () => {
+        btnArrow.classList.toggle('header-accord-arrow-active');
+        bottomPhoneNumber.classList.toggle('header-accord-active');
+        
+    });
+
+}
+
+export default main;
